f7180ca697479f28f2031ae4b6c071cf8d81394e
bcastel1 <bcastel1@msudenver.edu>
2022-02-11 23:31:39 -0700

    CRUD test - outline

--- Test.java
+++ Test.java
@@ -11,11 +11,11 @@ public class Test {
 
     }
     // CRUD Operation Tests 
-    // Tests that open and access SQL database 
+    // Tests that open and access SQL database
 
-    // Tests that reads from the SQL database and confirms is true 
+    // Tests that reads from the SQL database and confirms is true
 
-    // Tests new product, using the set properties of product ID, Qty, 
+    // Tests new product, using the set properties of product ID, Qty,
     // Sale price, Supplier ID
      
     // Tests that we can access and delete what was updated
