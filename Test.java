54929bee89a5fcc9ba12b66d244c711adf3dafc1
bcastel1 <bcastel1@msudenver.edu>
2022-02-11 23:30:01 -0700

    Merge pull request #3 from Team6-CS3250/Ciscoslahs-patch-1
    Add files via upload

---
+++ Test.java
@@ -0,0 +1,22 @@
+/* Testing is done by supplying a specific ProductID, Quantity, WholesaleCost,
+SalePrice, or SupplierID*/
+
+public class Test {
+
+    public static void main(String[] args){
+        //Test of .txt file
+        InventoryData test = new InventoryData();
+        //test.testFile();
+        test.viewInventory();
+
+    }
+    // CRUD Operation Tests 
+    // Tests that open and access SQL database 
+
+    // Tests that reads from the SQL database and confirms is true 
+
+    // Tests new product, using the set properties of product ID, Qty, 
+    // Sale price, Supplier ID
+     
+    // Tests that we can access and delete what was updated
+}
